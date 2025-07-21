ee6e9fb1c6c441aa9f895f5f49fca009ea1e4d7f
Any Task <t.series@yandex.ru>
2025-07-21 19:30:17 +0300

    Enhance fan control and relay functionality with improved timing and logging

--- wateringWithDisplay/wateringWithDisplay.ino
+++ wateringWithDisplay/wateringWithDisplay.ino
@@ -15,6 +15,17 @@ AHT10Class AHT10;
 unsigned long previousMillis = 0;
 const long interval = 1000; // 1 second interval
 
+// Relay timer variables
+unsigned long previousRelayMillis = 0;
+const long relayInterval = 2000; // 2 second interval
+bool relayState = false;
+
+// Fan speed timer variables
+unsigned long previousFanMillis = 0;
+const long fanInterval = 10000; // 10 second interval
+int fanSpeed = 10; // Start at 10%
+int fanStep = 10; // Increase by 10% each time
+
 
 // display
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -27,7 +38,8 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 void setup()
 {
   Serial.begin(115200);
-  analogWriteFreq(10000);
+  analogWriteFreq(2000);   // Set PWM frequency to 2kHz (better for fans)
+  analogWriteRange(1023);  // Set PWM range to 0-1023
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
     Serial.println(F("SSD1306 allocation failed"));
@@ -49,13 +61,52 @@ void setup()
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(relay_pin, OUTPUT);
+  pinMode(pwm_pin, OUTPUT);
   digitalWrite(relay_pin, 0);
   digitalWrite(LED_BUILTIN, HIGH);
+  
+  // Set initial fan speed to 10% (10% of 1023 = 102)
+  analogWrite(pwm_pin, 102);
 }
 
 void loop()
 {
   unsigned long currentMillis = millis();
+  
+  // Fan speed timer - change every 10 seconds
+  if (currentMillis - previousFanMillis >= fanInterval) {
+    previousFanMillis = currentMillis;
+    
+    // Calculate PWM value (fanSpeed% of 1023)
+    int pwmValue = (fanSpeed * 1023) / 100;
+    analogWrite(pwm_pin, pwmValue);
+    
+    Serial.print("Fan speed: ");
+    Serial.print(fanSpeed);
+    Serial.print("% (PWM: ");
+    Serial.print(pwmValue);
+    Serial.println(")");
+    
+    // Increase speed by 10%
+    fanSpeed += fanStep;
+    
+    // Reset to 10% when reaching 110%
+    if (fanSpeed > 100) {
+      fanSpeed = 10;
+    }
+  }
+  
+  // Relay timer - blink every 2 seconds
+  if (currentMillis - previousRelayMillis >= relayInterval) {
+    previousRelayMillis = currentMillis;
+    relayState = !relayState;
+    // digitalWrite(relay_pin, relayState);
+    digitalWrite(relay_pin, HIGH); // Turn relay on
+    Serial.print("Relay state: ");
+    Serial.println(relayState ? "ON" : "OFF");
+  }
+  
+  // Sensor reading timer - every 1 second
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
     
